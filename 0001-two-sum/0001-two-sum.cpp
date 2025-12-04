d376a85b23e923b6c97ccbe154a5dc8cd44b2430
Abdullah shahzad <145151091+Abdullahshaz70@users.noreply.github.com>
2025-12-04 20:12:30 +0500

    Time: 4 ms (53.95%), Space: 14.9 MB (19.97%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,31 +1,19 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-            int size = nums.size();
-        int s = 0;
-        int temp = s + 1;
-        bool haha = false;
-        vector<int> result;
-
-        while (s < size - 1) {
-            if (nums[s] + nums[temp] == target && temp != s) {
-                result.push_back(s);
-                result.push_back(temp);
-                haha = true;
-                break;
-            }
-            temp++;
-            if (temp == size) {
-                s++;
-                temp = s + 1;
-            }
-        }
-
-        if (!haha) {
-            cout << "NOT FOUND!" << endl;
-        }
-        
-        return result;
-    }
+    unordered_map<int,int> map;
     
+    for(int i = 0 ; i < nums.size() ; i++){
+        int needed = target - nums[i];
+
+        if(map.find(needed) != map.end())
+            return {map[needed] , i};
+        
+        map[nums[i]] = i;
+    }
+
+    return {};
+
+}
+
 };
