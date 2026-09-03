e3bb2ec7b1160fb5ecc25f0edacee0377ae539f7
Abdullah shahzad <145151091+Abdullahshaz70@users.noreply.github.com>
2026-09-03 21:41:34 +0500

    Time: 15 ms (69.52%), Space: 25.5 MB (61.28%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,46 +1,26 @@
 class Solution {
 public:
-    
-vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
     vector<vector<string>> sol;
 
-    vector<string> temp= strs;
-    int n = strs.size();
-    int index = 0;
-    
-    vector<bool> checker(n, false);
+    unordered_map< string, vector<string>> map;
 
-    for (int i = 0; i < n; i++)
-        sort(temp[i].begin(), temp[i].end());
-
-    for (int i = 0; i < n; i++) {
-
-        if (checker[i])
-            continue;
-        
-        sol.resize(sol.size() + 1);
-        sol[index].push_back(strs[i]);
-        checker[i] = true;
-        
-        for (int j = i + 1; j < n; j++) {
-            if (checker[j] == false and (temp[i] == temp[j])) {
-
-
-                //sol.resize(sol.size() + 1);
-
-                sol[index].push_back(strs[j]);
-
-                
-                checker[j] = true;
-            }
-
-        }
-
-        index++;
+    for (auto s : strs) {
+        string key = s;
+        sort(key.begin(), key.end());
+        map[key].push_back(s);
     }
+
+    sol.resize(map.size());
+    int idx = 0;
+    for (auto p : map) {
+        sol[idx] = p.second;
+        idx++;
+    }
+
+
 
 
     return sol;
 }
-
 };
